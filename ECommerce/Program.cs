58bc3f3dbdfa79ebc9dd87d728e4d1ccd6fd032e
TakeMeToChurch <136136690+Teemo789@users.noreply.github.com>
2024-11-22 22:26:23 +0100

    Update Program.cs

--- ECommerce/Program.cs
+++ ECommerce/Program.cs
@@ -21,7 +21,6 @@ builder.Services.AddScoped<IReductionCalculationService, ReductionCalculationSer
 
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
